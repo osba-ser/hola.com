831a4ead2d7631a70d806e02320b03830b09bc06
osba-ser <osba2005_@outlook.es>
2025-04-15 19:49:02 +0000

    [🌙] Backup realizado! 2025-04-15

--- msx (1).py
+++ msx (1).py
@@ -5,13 +5,7 @@
 
 # ================
 # MSX LAUNCHER 1.1
-# ================
-# 1. Instala la extensión de Python
-# 2. Haz click al botón de arriba a la derecha (►)
-
-# Si no aparece el botón, reinicia la página o cambia de navegador.
-
-
+# ================o
 
 
 
